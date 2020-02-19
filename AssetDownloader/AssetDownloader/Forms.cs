fcc0ba4df1f236e94f99fddcfb5df8428f3d2c11
Iris <39096994+IrisV3rm@users.noreply.github.com>
2020-02-19 16:49:27 -0700

    Initial commit

---
+++ AssetDownloader/AssetDownloader/Forms.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssetDownloader
+{
+    class Forms
+    {
+        public static DialogResult Notify(string Message, bool YN, bool OK)
+        {
+            return new Notice(Message, YN, OK).ShowDialog();
+        }
+    }
+}
